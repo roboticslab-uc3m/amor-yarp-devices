21e41b190204a0eacfe604c77d9191bc73b943be
Bartek Łukawski <PeterBowman@users.noreply.github.com>
2023-12-03 19:48:14 +0100

    Import AmorCartesianControl from roboticslab-uc3m/kinematics-dynamics

---
+++ libraries/YarpPlugins/AmorCartesianControl/AmorCartesianControl.cpp
@@ -0,0 +1,29 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#include "AmorCartesianControl.hpp"
+
+#include <cmath>
+
+#include <yarp/os/Log.h>
+
+#include "LogComponent.hpp"
+
+using namespace roboticslab;
+
+// -----------------------------------------------------------------------------
+
+bool AmorCartesianControl::checkJointVelocities(const std::vector<double> & qdot)
+{
+    for (unsigned int i = 0; i < qdot.size(); i++)
+    {
+        if (std::abs(qdot[i]) > qdotMax[i])
+        {
+            yCError(ACC, "Maximum angular velocity hit: qdot[%d] = %f > %f [deg/s]", i, qdot[i], qdotMax[i]);
+            return false;
+        }
+    }
+
+    return true;
+}
+
+// -----------------------------------------------------------------------------
